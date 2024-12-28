40be0b941391038a3963ebb06dfbaa7bf1291521
DR-2002 <digherohit101@gmail.com>
2024-12-28 17:59:08 +0530

    files uploaded

--- src/App.js
+++ src/App.js
@@ -1,10 +1,8 @@
 import Navbar from './components/Navbar';
 import './App.css';
 import TextForm from './components/TextForm';
-import About from './components/About';
 import { useState } from 'react';
 import Alert from './components/Alert';
-import { Route,Routes } from 'react-router-dom';
 
 function App() {
   const [mode,setMode]=useState('light');
@@ -46,11 +44,7 @@ function App() {
   <>
   <Navbar mode={mode} toggleMode={toggleModeHandler} title="TextUtils" aboutText="About" onBackgroundChange={backgroundHandler}/>
   <Alert alert={alert}/> 
-   <Routes>  
-    <Route path='/' element={<TextForm heading="Enter the text to analyze" mode={mode} showAlert={showAlert} Color={background}/>}/>
-    <Route path='/about' element={<About/>}/>
-    
-   </Routes>
+  <TextForm heading="Enter the text to analyze" mode={mode} showAlert={showAlert} Color={background}/>
    </>
   );
 }
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,5 +1,4 @@
 import React from "react";
-import { Link } from "react-router-dom";
 
 function Navbar(props) {
   return (
@@ -8,9 +7,9 @@ function Navbar(props) {
       data-bs-theme={props.mode}
     >
       <div className="container-fluid">
-        <Link className="navbar-brand" to="/">
+        <a className="navbar-brand" href="#">
           {props.title}
-        </Link>
+        </a>
         <button
           className="navbar-toggler"
           type="button"
@@ -25,15 +24,15 @@ function Navbar(props) {
         <div className="collapse navbar-collapse" id="navbarSupportedContent">
           <ul className="navbar-nav me-auto mb-2 mb-lg-0">
             <li className="nav-item">
-              <Link className="nav-link active" aria-current="page" to="/">
+              <a className="nav-link active" aria-current="page" href="#">
                 Home
-              </Link>
+              </a>
             </li>
-            <li className="nav-item">
-              <Link className="nav-link" to="/about">
+            {/* <li className="nav-item">
+              <a className="nav-link" href="#">
                 {props.aboutText}
-              </Link>
-            </li>
+              </a>
+            </li> */}
           </ul>
 
           {/* Pagination */}
